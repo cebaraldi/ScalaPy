10f769449326c7c0d0da0d224101b74bff57141b
cebaraldi <cebaraldi@gmail.com>
2024-03-02 15:02:43 +0100

    ScalaPy using 3.lts loaded to GitHub.

--- FTPutil/FTPUtil.scala
+++ FTPutil/FTPUtil.scala
@@ -7,6 +7,8 @@ import me.shadaj.scalapy.py.*
 import py.PyQuote
 import java.nio.file.{NoSuchFileException}
 
+// SEE: https://scalapy.dev/
+
 object FTPUtil {
 
   def pario(): String = {
@@ -41,9 +43,10 @@ object FTPUtil {
     val json = ujson.read(pario())
 
     // Global Scope
-    val list = py.Dynamic.global.range(1, 3 + 1)
-    val listSum = py.Dynamic.global.sum(list)
-    println(s"Global Scope: listSum = ${listSum}")
+//    val list = py.Dynamic.global.range(1, 3 + 1)
+//    val listSum = py.Dynamic.global.sum(list)
+//    println(s"Global Scope: listSum = ${listSum}")
+    py"print('Hello from Python!')"
 
     py.local { // python local scope
       val user = json("user").str
@@ -52,23 +55,32 @@ object FTPUtil {
       val local = json("local").str
       val remote = json("remote").str
 
+      // import Python module os
       val os = py.module("os")
-      os.chdir(local)
+
+      // import Python module ftputil
       val ftp = py.module("ftputil")
+//      SEE https://techbuddy.dev/scala-python-integration-datascience
+//      val ftp = py.Dynamic.global.selectDynamic("ftputil") NOT WORKING!
+
       py.`with`(ftp.FTPHost(server, user, pass)) { host =>
         host.chdir(remote)
         host.chdir("recent")
-        val files = host.listdir(host.curdir) // [11:22].toPythonCopy
+        val files = host.listdir(host.curdir)
         // println(files)
-        //files.
 
-        val f = files.bracketAccess(11)
-        val d = host.download_if_newer(f, f)
-        println(s"... ${d.getClass}")
-
-        //if (d === "False") println("something")
-        println(s"downloaded ${f}")
-        println(d)
+        // FTP download files from remote to local
+        os.chdir(local)
+        var count = 0
+        for (i <- 0 until py.Dynamic.global.len(files).as[Int]) {
+          val f = files.bracketAccess(i)
+          val downloaded = host.download_if_newer(f, f).as[Boolean]
+          if (downloaded) {
+            println(s"${f} newly downloaded")
+            count += 1
+          }
+        }
+        println(s"In total ${count} files downloaded.")
 
       }
     }
